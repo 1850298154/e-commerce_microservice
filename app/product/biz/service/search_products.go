2329a179dfb7346deb44556145bd493bee9ff2ea
Penryn <15158052130@163.com>
2025-01-20 16:44:50 +0800

    feat(product): 新增product的代码生成

---
+++ app/product/biz/service/search_products.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"context"
+
+	product "2501YTC/rpc_gen/kitex_gen/product"
+)
+
+type SearchProductsService struct {
+	ctx context.Context
+} // NewSearchProductsService new SearchProductsService
+func NewSearchProductsService(ctx context.Context) *SearchProductsService {
+	return &SearchProductsService{ctx: ctx}
+}
+
+// Run create note info
+func (s *SearchProductsService) Run(req *product.SearchProductsReq) (resp *product.SearchProductsResp, err error) {
+	// Finish your business logic.
+
+	return
+}
