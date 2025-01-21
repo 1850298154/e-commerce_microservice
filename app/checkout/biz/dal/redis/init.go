2c65e5a192c3faac1d0bacc0a0e36ae8b1ae4300
Penryn <15158052130@163.com>
2025-01-21 11:48:30 +0800

    chore(lint): 格式化代码

--- app/checkout/biz/dal/redis/init.go
+++ app/checkout/biz/dal/redis/init.go
@@ -3,13 +3,12 @@ package redis
 import (
 	"context"
 
-	"github.com/redis/go-redis/v9"
 	"2501YTC/app/checkout/conf"
+
+	"github.com/redis/go-redis/v9"
 )
 
-var (
-	RedisClient *redis.Client
-)
+var RedisClient *redis.Client
 
 func Init() {
 	RedisClient = redis.NewClient(&redis.Options{
