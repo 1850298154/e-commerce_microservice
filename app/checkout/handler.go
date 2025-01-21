29d46c072584f2901e034927a72c281388ab029b
yt z <57978791+1850298154@users.noreply.github.com>
2025-01-21 11:37:37 +0800

    feat(checkout): 生成checkout模块项目

---
+++ app/checkout/handler.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	checkout "2501YTC/rpc_gen/kitex_gen/checkout"
+	"context"
+	"2501YTC/app/checkout/biz/service"
+)
+
+// CheckoutServiceImpl implements the last service interface defined in the IDL.
+type CheckoutServiceImpl struct{}
+
+// Checkout implements the CheckoutServiceImpl interface.
+func (s *CheckoutServiceImpl) Checkout(ctx context.Context, req *checkout.CheckoutReq) (resp *checkout.CheckoutResp, err error) {
+	resp, err = service.NewCheckoutService(ctx).Run(req)
+
+	return resp, err
+}
