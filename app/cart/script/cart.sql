d089c9bfc7e4101fac1632401e5b5699d0a31455
guaguadanchaofan <150592318+guaguadanchaofan@users.noreply.github.com>
2025-01-23 02:08:45 +0800

    Revert "Zhaowei/0121/cart dev"

--- app/cart/script/cart.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `cart` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
-    `product_id` INT NOT NULL,
-    `product_num` INT NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    constraint pk_cart_id primary key (id)
-)
