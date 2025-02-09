21d12acd076b823ef6abefbc87028f684cfccdf6
zooos <2301213144@stu.pku.edu.cn>
2025-02-09 22:56:48 +0800

    feat(checkout): 需要拉去其他微服务的rpc进行集成测试

--- app/checkout/biz/service/checkout_test.go
+++ app/checkout/biz/service/checkout_test.go
@@ -2,17 +2,38 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	checkout "2501YTC/rpc_gen/kitex_gen/checkout"
+	payment "2501YTC/rpc_gen/kitex_gen/payment"
 )
 
 func TestCheckout_Run(t *testing.T) {
+	fmt.Println("TestCheckout_Run")
 	ctx := context.Background()
 	s := NewCheckoutService(ctx)
 	// init req and assert value
 
-	req := &checkout.CheckoutReq{}
+	req := &checkout.CheckoutReq{
+		UserId:    111,
+		Firstname: "testFirstname",
+		Lastname:  "testLastname",
+		Email:     "testEmail",
+		Address: &checkout.Address{
+			StreetAddress: "testStreetAddress",
+			City:          "testCity",
+			State:         "testState",
+			Country:       "testCountry",
+			ZipCode:       "testZipCode",
+		},
+		CreditCard: &payment.CreditCardInfo{
+			CreditCardNumber:          "testCreditCardNumber",
+			CreditCardCvv:             3,
+			CreditCardExpirationYear:  3,
+			CreditCardExpirationMonth: 3,
+		},
+	}
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
 	t.Logf("resp: %v", resp)
