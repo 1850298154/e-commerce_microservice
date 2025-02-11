9acc7f1345b646f3834b34e29ff7a6b240ec199c
Zjyhub <2313196292@qq.com>
2025-02-11 19:51:23 +0800

    chore(payment):格式化代码，添加链路追踪

--- app/payment/biz/dal/mysql/init.go
+++ app/payment/biz/dal/mysql/init.go
@@ -19,7 +19,6 @@ var (
 func Init() {
 	// dsn := "root:root@tcp(127.0.0.1:3306)/payment?charset=utf8mb4&parseTime=True&loc=Local"
 	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT"), os.Getenv("MYSQL_DATABASE"))
-	fmt.Println("dsn:", dsn)
 	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
