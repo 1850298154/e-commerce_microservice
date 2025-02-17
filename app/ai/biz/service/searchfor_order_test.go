5f7f760b2f6decec56350baab67a801dd1427cfa
2323398255@qq.com <2323398255@qq.com>
2025-02-18 00:32:42 +0800

    feat(ai): 生成ai客户端代码和服务端代码

---
+++ app/ai/biz/service/searchfor_order_test.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"context"
+	"testing"
+	ai "2501YTC/rpc_gen/kitex_gen/ai"
+)
+
+func TestSearchforOrder_Run(t *testing.T) {
+	ctx := context.Background()
+	s := NewSearchforOrderService(ctx)
+	// init req and assert value
+
+	req := &ai.SearchforOrderReq{}
+	resp, err := s.Run(req)
+	t.Logf("err: %v", err)
+	t.Logf("resp: %v", resp)
+
+	// todo: edit your unit test
+
+}
