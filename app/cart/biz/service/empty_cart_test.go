eff3e8bde07e602c2be9ded9a8189508c842311d
2323398255@qq.com <2323398255@qq.com>
2025-02-17 16:24:46 +0800

    feat(cart): 完善单元测试模块

--- app/cart/biz/service/empty_cart_test.go
+++ app/cart/biz/service/empty_cart_test.go
@@ -4,15 +4,20 @@ import (
 	"context"
 	"testing"
 
+	"2501YTC/app/cart/biz/dal"
+	"2501YTC/app/cart/infra/rpc"
 	cart "2501YTC/rpc_gen/kitex_gen/cart"
 )
 
 func TestEmptyCart_Run(t *testing.T) {
+	dal.Init()
+	rpc.InitClient()
 	ctx := context.Background()
 	s := NewEmptyCartService(ctx)
 	// init req and assert value
-
-	req := &cart.EmptyCartReq{}
+	req := &cart.EmptyCartReq{
+		UserId: 1,
+	}
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
 	t.Logf("resp: %v", resp)
