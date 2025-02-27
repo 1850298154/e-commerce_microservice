9a837f9eff2428850c5dd2d7142d463d0224f9b8
rdn <1923290842@qq.com>
2025-02-27 17:47:35 +0800

    test(user): 完善了user的测试用例

--- app/gateway/biz/utils/errors.go
+++ app/gateway/biz/utils/errors.go
@@ -1,9 +1,19 @@
 package utils
 
-import "github.com/cloudwego/hertz/pkg/common/hlog"
+import (
+	"fmt"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/pkg/errors"
+)
 
 func MustHandleError(err error) {
 	if err != nil {
 		hlog.Fatal(err)
 	}
+}
+
+const ()
+
+func NewBizErr(errCode int64, errMsg string) error {
+	return errors.New(fmt.Sprintf("err_code=%d, err_msg=%s", errCode, errMsg))
 }
